1cea967dcd8e91f9427f495f4323a0fb90c9994c
luisfelipegomesr <luisfelipegomesr@gmail.com>
2023-06-29 18:36:56 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,23 +4,17 @@ import Freelancers from "./components/Freelancers"
 import QuemSomos from './components/QuemSomos'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <React.StrictMode>
-      <Switch>
-        <Route path='/'>
-          <Home />
-        </Route>
-        <Route path='/freelancers'>
-          <Freelancers />
-        </Route>
-        <Route path='/quemsomos'>
-          <QuemSomos />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path='/' Component={Home}/>
+        <Route path='/freelancers'Component={Freelancers}/>
+        <Route path='/quemsomos' Component={QuemSomos}/>
+      </Routes>
     </React.StrictMode>
   </Router>
 );
