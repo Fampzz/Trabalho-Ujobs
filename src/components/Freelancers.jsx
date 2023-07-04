afbc5abf2b16b6a61cf70aca6cc3ce536bba636c
luisfelipegomesr <luisfelipegomesr@gmail.com>
2023-07-04 01:28:42 -0300

    0.3

--- src/components/Freelancers.jsx
+++ src/components/Freelancers.jsx
@@ -1,27 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Style.css'
-function Freelancers(){ 
+function Freelancers() {
+    const [freelas, setFreelas] = useState([]);
+    const url = "https://api-black-sigma.vercel.app/freelancers";
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => setFreelas(data))
+            .catch((err) => console.log(err))
+    },[]);
     return (
         <div>
-            <nav class="cabecalho">
+            <nav className="cabecalho">
                 <div>
-                    <a href='/'> <img class="logo-img" src={require("../assets/logos/Logo.png")} alt='' /> </a>
-                    <div class="menu-superior">
+                    <a href='/'> <img className="logo-img" src={require("../assets/logos/Logo.png")} alt='' /> </a>
+                    <div className="menu-superior">
                         <nav>
-                            <a class="menu-text" href="/freelancers">Freelancers</a>
-                            <a class="menu-text" href="/quem-somos">Quem somos?</a>
+                            <a className="menu-text" href="/freelancers">Freelancers</a>
+                            <a className="menu-text" href="/quem-somos">Quem somos?</a>
                         </nav>
                     </div>
                 </div>
             </nav>
-            <div class="auncios" id="anunciosID">
-                <div class="card-anuncio">
-                    <div class="card-content">
-                        <img class="img-card" src={require("../assets/ProfilePics/user1.jpg")} alt='' />
-                        <div class="dados-card">
-                            <h2 class="nome-card">Pedro Pascal</h2>
-                            <p class="cargo-card">Sobrevivente</p>
-                            <p class="localizacao">Rio de Janeiro - RJ</p>
+            <div className="auncios" id="anunciosID">
+                <ul>
+                    {freelas.map(freelas => (
+                        <li>
+                            <div className="card-anuncio">
+                                <div className="card-content">
+                                    <img className="img-card" src={require("../assets/ProfilePics/user1.jpg")} alt='' />
+                                    <div className="dados-card">
+                                        <h2 className="nome-card">{freelas.nome}</h2>
+                                        <p className="cargo-card">{freelas.profissao}</p>
+                                        <p className="localizacao">{freelas.cidade}</p>
+                                        <p></p>
+                                    </div>
+                                </div>
+                                <div className="card-contato">
+                                    <button className="btns btns-card" /*onClick={}*/>
+                                        Contrate
+                                    </button>
+                                    <p className="text-card-contato">Preço por hora:</p>
+                                    <h3 className="price-card-contato">R$ {freelas.preco}</h3>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+                {/* <div className="card-anuncio">
+                    <div className="card-content">
+                        <img className="img-card" src={require("../assets/ProfilePics/user1.jpg")} alt='' />
+                        <div className="dados-card">
+                            <h2 className="nome-card">Pedro Pascal</h2>
+                            <p className="cargo-card">Sobrevivente</p>
+                            <p className="localizacao">Rio de Janeiro - RJ</p>
                             <p>Olá, este é um exemplo de texto escrito por um freelancer. Os freelancers são profissionais
                                 autônomos que oferecem seus serviços de forma independente.</p>
                         </div>
@@ -116,7 +149,7 @@ function Freelancers(){
                         <p class="text-card-contato">Preço por hora:</p>
                         <h2 class="price-card-contato">R$ 00,00</h2>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
